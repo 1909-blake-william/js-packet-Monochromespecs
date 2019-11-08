e5df078a14854174f85b51639f93c23303fbf928
Joshua O'Silas <josmecian@gmail.com>
2019-11-08 02:30:44 -0500

    changes to html and js files

---
+++ packet.js
@@ -0,0 +1,127 @@
+// Javascript Homework
+// Due next Friday Morning
+// -----------------------------------------------------------------------------------
+// PART I
+//
+// Create a single Javascript file called packet.js to answer these questions.
+// Please put the question itself as a comment above each answer.
+// -----------------------------------------------------------------------------------
+//
+// 1. Fibonacci
+// Define function: fib(n)
+// Return the nth number in the fibonacci sequence.
+
+
+
+// 2. Bubble Sort
+// Define function: bubbleSort(numArray)
+// Use the bubble sort algorithm to sort the array.
+// Return the sorted array.
+
+
+
+// 3. Reverse String
+// Define function: reverseStr(someStr)
+// Reverse and return the String.
+
+
+
+// 4. Factorial
+// Define function: factorial(someNum)
+// Use recursion to compute and return the factorial of someNum.
+
+
+
+// 5. Substring
+// Define function substring(someStr, length, offset)
+// Return the substring contained between offset and (offset + length) inclusively.
+// If incorrect input is entered, use the alert function and describe why the input was incorrect.
+
+
+
+// 6. Even Number
+// Define function: isEven(someNum)
+// Return true if even, false if odd.
+// Do not use % operator.
+
+
+// 7. Palindrome
+// Define function isPalindrome(someStr)
+// Return true if someStr is a palindrome, otherwise return false
+
+
+// 8. Shapes
+// Define function: printShape(shape, height, character)
+// shape is a String and is either "Square", "Triangle", "Diamond".
+// height is a Number and is the height of the shape. Assume the number is odd.
+// character is a String that represents the contents of the shape. Assume this String contains just one character.
+// Use a switch statement to determine which shape was passed in.
+// Use the console.log function to print the desired shape.
+// Example for printShape("Square", 3, "%");
+// %%%
+// %%%
+// %%%
+// Example for printShape("Triangle", 3, "$");
+// $
+// $$
+// $$$
+// Example for printShape("Diamond", 5, "*");
+//   *
+//  ***
+// *****
+//  ***
+//   *
+
+
+// 9. Object literal
+// Define function traverseObject(someObj)
+// Print every property and it's value.
+function traverseObject(someObj){
+    for (let i = 0;i < someObj.length; i++){
+        console.log("property" + i + " and " + someObj[i]);
+    }
+    
+}
+
+// 10. Delete Element
+// Define function deleteElement(someArr)
+// Print length
+// Delete the third element in the array.
+// Print length
+// The lengths should be the same.
+function deleteElement(someArr){
+    console.log(someArr.length);
+    someArr.length += someArr.length-2;
+    console.log(someArr.length);
+}
+
+// 11. Splice Element
+// Define function spliceElement(someArr)
+// Print length
+// Splice the third element in the array.
+// Print length
+// The lengths should be one less than the original length.
+function spliceElement(someArr){
+    console.log(someArr.length);
+    someArr.spliceElement(someArr.length-1);
+    console.log(someArr.length);
+}
+
+// 12. Defining an object using a constructor
+// Define a function Person(name, age)
+// The following line should set a Person object to the variable john:
+// 	let john = new Person("John", 30);
+Person = new Object();
+let john = new Person();
+john.name = "John";
+john.age = 30;
+
+// 13. Defining an object using an object literal
+// Define function getPerson(name, age)
+// The following line should set a Person object to the variable john:
+// 	let john = getPerson("John", 30);
+Person = {
+    name:"John",
+    age:30
+};
+let john = getPerson("John", 30);
